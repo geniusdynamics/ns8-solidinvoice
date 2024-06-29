e166a65eaa26a241917ef00226db45a9a96d5adc
kemboi22 <kemboielvis22@gmail.com>
2024-06-29 04:41:28 +0300

    removed using full build

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/solidinvoice/solidinvoice:2.2.6-full" \
+    --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/solidinvoice/solidinvoice:2.2.6" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
